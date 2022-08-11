f20e494d5c7fac8e91c442da5cda0ce1e58e1796
Mothusi Maemo <maemomothusi@gmail.com>
2022-08-11 12:15:05 -0700

    keep

--- paygateway/settings.py
+++ paygateway/settings.py
@@ -119,8 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'statictfiles')
+
 STATIC_URL = 'static/'
+
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
